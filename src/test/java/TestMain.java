a0f54509e92ef10587870404bff2f7a8f4f45a76
Igor <lyakhov@proitr.ru>
2022-03-09 22:55:48 +0300

    Sex1

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,8 +1,10 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMain {
     @Test
     public void testMethod(){
         First.sayHello();
+        Assert.assertNotNull(null);
     }
 }
